9d6cfe6f5ae0c7a8b91868627fbbc901f28df8fe
ZanyaMyers <myerszanya@gmail.com>
2023-11-03 09:12:34 -0700

    variableExercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace VariablesExercise
-{
+{ 
     public class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        string name = "Zanya Myers";
+        int age = 22;
+        char middleItinial = 'A';
+        bool isCool = true;
+        double randomNumber = 30.5;
+        decimal priceOfCoffee = 5.45m;
+
+
+        Console.WriteLine($"My name is {name},I am {age}. My coffee is {priceOfCoffee} everyday");
+
     }
+}
 }
